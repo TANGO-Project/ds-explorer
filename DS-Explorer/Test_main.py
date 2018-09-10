859052c1c95f3b3c9554aed8a24049e63ee07aaa
Badrinarayanan Venkatakrishnan <bv@cetic.be>
2018-09-10 09:51:20 +0200

    Include write to yaml file based on user input (error occurs during writing)
    Signed-off-by: Badrinarayanan Venkatakrishnan <bv@cetic.be>

--- DS-Explorer/Test_main.py
+++ DS-Explorer/Test_main.py
@@ -1,7 +1,8 @@
 import os
 import re
+import fileinput
 
-path = "C:\RH7_Shared\Clone_SobelPiP_50MHz & 8 bits\impl\implementation_0"  # Location of the yaml files
+path = "C:\RH7_Shared\Clone_SobelPiP_50Mhz & 8 bits\impl\implementation_0"  # Location of the yaml files
 fileList = os.listdir(path)
 count = 0
 yamlList = []
@@ -17,8 +18,8 @@ print {"Number of yaml files = %d" % count}
 for each in yamlList:
     yamlFile = path + '\\' + yamlList[i]
     currentFile = open(yamlFile)
-# Go through each line of the yaml file. The line following the "property: width" line stores the value of the width
-# and thus it can be extracted. Similarly the values of clock frequency & FIFO width can also be extracted.
+    # Go through each line of the yaml file. The line following the "property: width" line stores the value of the width
+    # and thus it can be extracted. Similarly the values of clock frequency & FIFO width can also be extracted.
     for line in currentFile:
         prop = re.match("^\s*- property:\s*(.+)\s*$", line)
         if prop:
@@ -28,7 +29,7 @@ for each in yamlList:
                 findValue = re.match("^\s*value:\s*(.+)\s*$", value)
                 if findValue:
                     retWidth = findValue.group(1)
-    # Parse and find clock frequency value
+        # Parse and find clock frequency value
         prop1 = re.match("^\s*- property:\s*(.+)\s*$", line)
         if prop1:
             prop1Is = prop1.group(1)
@@ -37,7 +38,7 @@ for each in yamlList:
                 findValue1 = re.match("^\s*value:\s*(.+)\s*$", value1)
                 if findValue1:
                     retFreq = findValue1.group(1)
-    # Parse and find FIFO width value
+        # Parse and find FIFO width value
         prop2 = re.match("^\s*- property:\s*(.+)\s*$", line)
         if prop2:
             prop2Is = prop2.group(1)
@@ -45,8 +46,8 @@ for each in yamlList:
                 value2 = currentFile.next()
                 findValue2 = re.match("^\s*value:\s*(.+)\s*$", value2)
                 if findValue2:
-                    retFIFO = findValue2.group(1)
-#                    print("The FIFO size is %s in file: " % retFIFO + yamlList[i])
+                    retFIFO = findValue2.group(1)  # type: str
+                    # print("The FIFO size is %s in file: " % retFIFO + yamlList[i])
     i = i + 1
 print("The byte width is %s" % retWidth)
 print("The clock frequency is %s" % retFreq)
@@ -55,34 +56,98 @@ print("The FIFO size is %s" % retFIFO)
 input1 = raw_input("Would you like to change the byte width?(Yes/No): ")
 input1 = input1.lower()
 if "yes" in input1:
+    widthChanged = 1
     print("The byte width takes the following values: 8, 16, 32, 64")
     newWidth = raw_input("Enter the new byte width: ")
-    if newWidth is not ['8', '16', '32', '64']:
+    while newWidth not in ['8', '16', '32', '64']:
         print("Please select a value from the given options")
         newWidth = raw_input("Enter the new byte width: ")
     width = newWidth
 else:
     width = retWidth
+    widthChanged = 0
 input2 = raw_input("Would you like to change the clock frequency?(Yes/No): ")
 input2 = input2.lower()
 if "yes" in input2:
+    freqChanged = 1
     print("The clock frequency takes the following values: 50, 75, 100, 200")
     newFreq = raw_input("Enter the new clock frequency: ")
-    if newFreq is not ['50', '75', '100', '200']:
+    while newFreq not in ['50', '75', '100', '200']:
         print("Please select a value from the given options")
         newFreq = raw_input("Enter the new clock frequency: ")
     freq = newFreq
 else:
     freq = retFreq
+    freqChanged = 0
 input3 = raw_input("Would you like to change the FIFO size?(Yes/No): ")
 input3 = input3.lower()
 if "yes" in input3:
+    fifoChanged = 1
     print("The byte width takes the following values: 2048, 4096, 14400, 230400")
     newFifoSize = raw_input("Enter the new FIFO size: ")
-    if newFifoSize is not ['2048', '4096', '14400', '230400']:
+    while newFifoSize not in ['2048', '4096', '14400', '230400']:
         print("Please select a value from the given options")
         newFifoSize = raw_input("Enter the new FIFO size: ")
     fifo = newFifoSize
 else:
     fifo = retFIFO
+    fifoChanged = 0
 print("Byte width: %s\n" % width + "Frequency: %s\n" % freq + "FIFO Size: %s" % fifo)
+
+print(widthChanged)
+print(freqChanged)
+print(fifoChanged)
+# Make the changes in the yaml files
+i = 0
+if widthChanged or freqChanged or fifoChanged:
+    print("Config changed")
+    for each in yamlList:
+        yamlFile = path + '\\' + yamlList[i]
+        currentFile = open(yamlFile, 'r+')
+        if widthChanged:
+            for line in currentFile:
+                prop = re.match("^\s*- property:\s*(.+)\s*$", line)
+                if prop:
+                    propIs = prop.group(1)
+                    if 'width' in propIs:
+                        value = currentFile.next()
+                        findValue = re.match("^\s*value:\s*(.+)\s*$", value)
+                        if findValue:
+                            retWidth = findValue.group(1)
+                            changeWidth = re.sub(retWidth, width, value)
+                            print(changeWidth)
+                            a = 'value: ' + retWidth
+           # currentFile.seek(0)
+           # for line in currentFile:
+                if a not in line:
+                    currentFile.write(line)
+                else:
+                    currentFile.write(changeWidth)
+            #currentFile.truncate()
+        # Parse and find clock frequency value
+        # elif freqChanged:
+        #     for line in currentFile:
+        #         prop1 = re.match("^\s*- property:\s*(.+)\s*$", line)
+        #         if prop1:
+        #             prop1Is = prop1.group(1)
+        #             if 'clockFrequency' in prop1Is:
+        #                 value1 = currentFile.next()
+        #                 findValue1 = re.match("^\s*value:\s*(.+)\s*$", value1)
+        #                 if findValue1:
+        #                     retFreq = findValue1.group(1)
+        #                     changeFreq = re.sub(retFreq, freq, value1)
+        #                     print(changeFreq)
+        # # Parse and find FIFO width value
+        # elif fifoChanged:
+        #     for line in currentFile:
+        #         prop2 = re.match("^\s*- property:\s*(.+)\s*$", line)
+        #         if prop2:
+        #             prop2Is = prop2.group(1)
+        #             if 'size' in prop2Is:
+        #                 value2 = currentFile.next()
+        #                 findValue2 = re.match("^\s*value:\s*(.+)\s*$", value2)
+        #                 if findValue2:
+        #                     retFIFO = findValue2.group(1)
+        #                     changeFIFO = re.sub(retFIFO, fifo, value2)
+        #                     print(changeFIFO)
+        i = i + 1
