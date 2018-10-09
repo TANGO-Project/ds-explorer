be0a2c7ee95da544a5b359187b36d8c044010b19
Badrinarayanan Venkatakrishnan <bv@cetic.be>
2018-10-09 12:06:56 +0200

    Included command line commands for compilation, build and implementation

--- DS-Explorer/Main.py
+++ DS-Explorer/Main.py
@@ -2,6 +2,7 @@ import shutil
 import File_Parsing
 import Report_parsing
 import datetime
+import os
 
 ts = str(datetime.datetime.now())      # Get timestamp and make it convert it to str type
 ts = filter(str.isalnum, ts)           # Remove special characters from timestamp so that it can be used in folder name
@@ -13,8 +14,11 @@ print("Files backed up\n")
 
 a = File_Parsing.FileParse(sourcePath)
 
-# TO-DO
-# Include code for command line execution of compilation, build and implementation of design
+# Command line execution for compilation, build and implementation of design
+
+comp = os.system("quickplay -cli compile --design=/home/lg/workspace/Clone_SobelPiP")
+build = os.system("quickplay -cli build --design=/home/lg/workspace/Clone_SobelPiP --implemntation=implementation_0")
+impl = os.system("quickplay -cli implement --design=/home/lg/workspace/Clone_SobelPiP --implemntation=implementation_0")
 
 # Location of reports generated after the design is implemented
 reportsPath = "C:\RH7_Shared\Clone_SobelPiP_50Mhz & 8 bits\implementations\implementation_0\\reports"
